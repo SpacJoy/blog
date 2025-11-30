f227e1edd63acf9d65756362f82618235fc3b0a9
Spacejoy <89252350+SpacJoy@users.noreply.github.com>
2025-11-30 23:37:17 +0800

    更新 banner 图片链接地址
    将 banner 图片的 src 链接由 rad.ysy.spacjoy.top 更改为 rad.spacjoy.top，统一资源域名，确保图片正常加载。

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "https://rad.ysy.spacjoy.top/bz/hp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "https://rad.spacjoy.top/bz/hp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
